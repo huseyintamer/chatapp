7b01838ac9cd87e259bf45f5cde0a3b289827edc
Hüseyin Tamer <huseyintamer0@gmail.com>
2020-11-14 18:18:17 +0300

    Socket IO Bug Fixed

--- BackEndSocketIO/server.js
+++ BackEndSocketIO/server.js
@@ -1,1 +1,15 @@
-var io = require('socket.io')(2020);
+var express = require('express');
+var app = express();
+var server = require('http').Server(app)
+var io = require('socket.io')(server);
+
+
+app.get('/', function (req, res){
+  res.sendFile(__dirname + '/index.html');
+ });
+
+ io.on('connection', function (socket) {
+   console.log("baglandi");
+  socket.on('update', (data) => io.emit('update', {data}));
+ });
+ server.listen(3000);
--- ChatApp/App.js
+++ ChatApp/App.js
@@ -1,21 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-export default function App() {
+
+import React,{useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  TextInput,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import io from 'socket.io-client/dist/socket.io';
+
+
+const App: () => React$Node = () => {
+
+const [message, setMessage ] =useState("");
+
+  const connectionConfig = {
+    jsonp: false,
+    reconnection: true,
+    reconnectionDelay: 100,
+    reconnectionAttempts: 100000,
+    transports: ['websocket'], 
+   };
+
+  useEffect(()=> {
+   const socket = io("http://192.168.1.34:3000", connectionConfig);
+   socket.on('connect', function(){
+    console.log('Socket connected!');
+  });
+  })
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+ 
+  <View style ={{flex:1,justifyContent:'center',alignItems:'center'}}>
+    <Text>hello world</Text>
+    <TextInput 
+    value={message}
+    onChangeText={text => setMessage(text)}
+    placeholder="Text Input" />
+ 
+  </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
   },
 });
+
+export default App;
